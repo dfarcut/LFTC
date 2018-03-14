59b87ee3984790c628358d6d5cb5b914d87ba99d
dfarcut <dfarcut@gmail.com>
2018-03-14 12:25:48 +0200

    added more states

--- analizor.c
+++ analizor.c
@@ -50,6 +50,26 @@ int getNextTK()
                 else if(isdigit(ch)&&(ch>='1'&& ch<='9')){s=3;pch++;}
                 else if(isdigit(ch)&& ch=='0'){s=5; pch++;}
                 else if(ch=='\''){s=16;pch++;}
+                else if(ch=='\"'){s=20;pch++;}
+                else if(ch==','){s=23;pch++;}
+                else if(ch==';'){s=24;pch++;}
+                else if(ch=='('){s=25;pch++;}
+                else if(ch==')'){s=26;pch++;}
+                else if(ch=='['){s=27;pch++;}
+                else if(ch==']'){s=28;pch++;}
+                else if(ch=='{'){s=29;pch++;}
+                else if(ch=='}'){s=30;pch++;}
+                else if(ch=='+'){s=31;pch++;}
+                else if(ch=='-'){s=32;pch++;}
+                else if(ch=='*'){s=33;pch++;}
+                else if(ch=='/'){s=34;pch++;}
+                else if(ch=='.'){s=35;pch++;}
+                else if(ch=='&'){s=54;pch++;}
+                else if(ch=='|'){s=55;pch++;}
+                else if(ch=='!'){s=38;pch++;}
+                else if(ch=='='){s=39;pch++;}
+                else if(ch=='<'){s=44;pch++;}
+                else if(ch=='>'){s=47;pch++;}
                 else printf("eroare \n");
                 break;
             case 1:if(isalnum(ch)||ch=='_'){pch++;}
@@ -62,7 +82,7 @@ int getNextTK()
                  else if(ch=='e'||ch=='E'){s=12;pch++;}
                  else s=4;
                  break;
-            case 4:addtk(CT_INT)
+            case 4:addtk(CT_INT);
                 return CT_INT;
             case 5:if(isalpha(ch) && ch=='x'){s=7; pch++;}
                    else if(ch>='0' && ch<='7'){s=6;pch++;}
@@ -70,7 +90,7 @@ int getNextTK()
                    break:
             case 6:if(ch>='0' && ch<='7'){pch++;}
             else if(ch=='8'||ch=='9'){s=9;pch++;}
-            else if(ch=='.'){s=10'pch++;}
+            else if(ch=='.'){s=10;pch++;}
             else if(ch=='e'||ch=='E'){s=12;pch++;}
             else s=4;
             break;
@@ -97,8 +117,100 @@ int getNextTK()
             else
             s=15;
             break;
-            case 15:addtk(CT_REAL)
+            case 15:addtk(CT_REAL);
                 return CT_REAL;
+            case 16:if(ch=='\\'){s=17;pch++}
+            else if(ch!='\\'||ch!='\''){s=18;pch++;}
+            break;
+            case 17:if(ch=='a'||ch=='b'||ch=='f'||ch=='n'||
+            ch=='r'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='\\'||ch=='0'||ch=='\"'){s=18;pch++;}
+            break;
+            case 18:if(ch=='\''){s=19;pch++;}
+            break;
+            case 19:addtk(CT_CHAR);
+            return CT_CHAR;
+            case 20:if(ch=='\\'){s=21;pch++;}
+            else if(ch=='\"'){s=22;pch++;}
+            else {s=20;pch++;}
+            break;
+            case 21:if(ch=='a'||ch=='b'||ch=='f'||ch=='n'||
+            ch=='r'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='\\'||ch=='0'||ch=='\"'){s=20;pch++;}
+            break;
+            case 22:addtk(CT_STRING);
+            return CT_STRING;
+            case 23:addtk(COMMA);
+            return COMMA;
+            case 24:addtk(SEMICOLON);
+            return SEMICOLON;
+            case 25:addtk(LPAR);
+            return LPAR;
+            case 26:addtk(RPAR);
+            return RPAR;
+            case 27:addtk(LBRACKET);
+            return LBRACKET;
+            case 28:addtk(RBRACKET);
+            return RBRACKET;
+            case 29:addtk(LACC);
+           return LACC;
+           case 30:addtk(RACC);
+           return RACC;
+           case 31:addtk(ADD);
+           return ADD;
+           case 32:addtk(SUB);
+           return SUB;
+           case 33:addtk(MUL);
+           return MUL;
+           case 34:if(ch=='/'){s=51;pch++;}
+           else if(ch=='*'){s=52;pch++;}
+           else s=50;
+           break;
+           case 35:addtk(DOT);
+           return DOT;
+           case 36:addtk(AND);
+           return AND;
+           case 37:addtk(OR);
+           return OR;
+           case 38:if(ch=='='){s=43;pch++;}
+           else
+           s=42;
+           break;
+           case 39:if(ch=='='){s=40;pch++;}
+           else s=41;
+           break;
+           case 40:addtk(EQUAL);
+           return EQUAL;
+           case 41:addtk(ASSIGN);
+           return ASSIGN;
+           case 42:addtk(NOT);
+           return NOT;
+           case 43:addtk(NOTEQ);
+           return NOTEQ;
+           case 44:if(ch=='='){s=46;pch++;}
+           else s=45;
+           break;
+           case 45:addtk(LESS);
+           return LESS;
+           case 46:addtk(LESSEQ);
+           return LESSEQ;
+           case 47:if(ch=='='){s=49;pch++;}
+           else s=48;
+           break;
+           case 48:addtk(GREATER);
+           return GREATER;
+           case 49:addtk(GREATEREQ);
+           return GREATEREQ;
+           case 50:addtk(DIV);
+           return DIV;
+           case 51:if(ch=='\n'||ch=='\r'\\ch!='\0')
+           s=0;
+           else
+           pch++;
+           break;
+           case 52:if(ch=='*'){s=53;pch++;}
+           else {pch++;break;}
+
+
+
             default:printf("Stare netratata %d \n",s);
         }
     }
@@ -106,7 +218,7 @@ int getNextTK()
 
 
 int main(int argc,char **argv)
-{  
+{
     char fisier;
     strcpy(fisier,argv[1]);
     char inbuff[30001];
